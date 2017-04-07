0530517fc6e9d92f81fd5e51c0fb020919eced84
zhengyi_i <zhengyi_i@didichuxing.com>
2017-04-07 18:30:05 +0800

    first commit: base

---
+++ cmake-build-debug/CMakeFiles/MyTestHttpModule.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyTestHttpModule.dir/main.c.o"
+  "MyTestHttpModule.pdb"
+  "MyTestHttpModule"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MyTestHttpModule.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
