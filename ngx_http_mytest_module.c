cbc37c0bf293addd50bb104678270514aee58130
zhengyi_i <zhengyi_i@didichuxing.com>
2017-04-07 18:59:40 +0800

    reformat

--- ngx_http_mytest_module.c
+++ ngx_http_mytest_module.c
@@ -1,9 +1,8 @@
 #include <ngx_config.h>
 #include <ngx_module.h>
 #include <ngx_http.h>
-#include <ngx_core.h>
 
-static char* ngx_http_mytest(ngx_conf_t *cf, ngx_command_t *cmd, void *conf) {
+static char *ngx_http_mytest(ngx_conf_t *cf, ngx_command_t *cmd, void *conf) {
     ngx_http_core_loc_conf_t *clcf;
     clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
     clcf->handler = ngx_http_mytest_handler;
@@ -14,7 +13,7 @@ static char* ngx_http_mytest(ngx_conf_t *cf, ngx_command_t *cmd, void *conf) {
 static ngx_command_t ngx_http_mytest_commands[] = {
     {
         ngx_string("mytest"),
-        NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_HTTP_LMT_CONF|NGX_CONF_NOARGS,
+        NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LMT_CONF | NGX_CONF_NOARGS,
         ngx_http_mytest,
         NGX_HTTP_LOC_CONF_OFFSET,
         0,
@@ -25,29 +24,13 @@ static ngx_command_t ngx_http_mytest_commands[] = {
 
 
 static ngx_http_module_t ngx_http_mytest_module_ctx = {
-    NULL,
-    NULL,
-
-    NULL,
-    NULL,
-
-    NULL,
-    NULL,
-
-    NULL,
-    NULL
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL
 };
 
 ngx_module_t ngx_http_mytest_module = {
     NGX_MODULE_V1,
     &ngx_http_mytest_module_ctx,
     ngx_http_mytest_commands,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL,
     NGX_MODULE_V1_PADDING
 };
